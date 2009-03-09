30f9fee5e673150f84a8a84cbd9c873e57ac2a58
flores.leonardo <flores.leonardo@986cc4a0-e254-0410-b11a-59b74a07c1a1>
2009-03-09 22:11:24 +0000

    Agrego la logica para poder parsear los datos del path filename,extension, y locale

--- com.triadsoft.properties.editor/src/com/triadsoft/properties/model/utils/WildcardPath.java
+++ com.triadsoft.properties.editor/src/com/triadsoft/properties/model/utils/WildcardPath.java
@@ -1,12 +1,15 @@
 package com.triadsoft.properties.model.utils;
 
+import java.io.File;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * Esta clase es la encargada de encapsular todos la logica para poder parsear y
- * descubrir los datos referidos al path
+ * descubrir los datos referidos al path La clase no maneja archivos sino que
+ * solamente manipula strings y obtiene los datos que serán utilizados por el
+ * controlador de archivos
  * 
  * @author triad
  */
@@ -16,6 +19,12 @@ public class WildcardPath {
 	public static final String FILE_EXTENSION_WILDCARD = "{fileextension}";
 	public static final String COUNTRY_WILDCARD = "{country}";
 	public static final String LANGUAGE_WILDCARD = "{lang}";
+
+	private String root;
+	private String fileName;
+	private String fileExtension;
+	private String country;
+	private String language;
 
 	private String wildcardpath;
 	private String path = "";
@@ -29,16 +38,105 @@ public class WildcardPath {
 		return wildcardpath;
 	}
 
-	public void setWildcardpath(String wildcardpath) {
-		this.wildcardpath = wildcardpath;
-	}
-
 	public String getPath() {
 		return path;
 	}
 
+	public String getRoot() {
+		return root;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public String getFileExtension() {
+		return fileExtension;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public Locale getLocale() {
+		Locale locale = null;
+		if (this.language != null && this.country == null) {
+			locale = new Locale(this.language);
+		} else if (this.language != null && this.country != null) {
+			locale = new Locale(this.language, this.country);
+		}
+		return locale;
+	}
+
 	/**
-	 * This method replace the language and country into the path string 
+	 * Este metodo se encarga de obtener del path pasado como parámetro los
+	 * datos correspondientes a cada wildcard
+	 */
+	public Boolean parse(String filepath) {
+		String escapedFilepath = escapedFilepath(filepath);
+		String wildcardRegex = this.replaceToRegex().getPath();
+		Pattern p = Pattern.compile(wildcardRegex);
+		Matcher m = p.matcher(escapedFilepath);
+		if (m.find()) {
+			String discoveredPath = escapedFilepath.substring(m.start(), m
+					.end());
+			String[] wildcards = wildcardpath.split("/");
+			String[] segments = discoveredPath.split("/");
+			for (int i = 0; i < segments.length; i++) {
+				System.out.println(segments[i] + ":" + wildcards[i]);
+				parseWildcard(wildcards[i], segments[i]);
+			}
+			if (this.language == null || this.country == null) {
+				throw new RuntimeException("Unparsable lang and country");
+			}
+			return true;
+		}
+		return false;
+	}
+
+	private Boolean parseWildcard(String wildcard, String segment) {
+		String[] segments = segment.split("\\.");
+		if (segments.length > 1) {
+			String[] wildcards = wildcard.split("\\.");
+			for (int i = 0; i < wildcards.length; i++) {
+				parseWildcard(wildcards[i], segments[i]);
+			}
+			return true;
+		}
+
+		segments = segment.split("\\_");
+		if (segments.length > 1) {
+			String[] wildcards = wildcard.split("\\_");
+			for (int i = 0; i < wildcards.length; i++) {
+				parseWildcard(wildcards[i], segments[i]);
+			}
+			return true;
+		}
+
+		// Tengo que ver si no viene algo complejo
+		if (wildcard.equals(ROOT_WILDCARD)) {
+			root = segment;
+		} else if (wildcard.equals(FILE_EXTENSION_WILDCARD)) {
+			fileExtension = segment;
+		} else if (wildcard.equals(FILENAME_WILDCARD)) {
+			fileName = segment;
+		} else if (wildcard.equals(COUNTRY_WILDCARD)) {
+			country = segment;
+		} else if (wildcard.equals(LANGUAGE_WILDCARD)) {
+			language = segment;
+		} else {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * This method replace the language and country into the path string
+	 * 
 	 * @param locale
 	 * @return
 	 */
@@ -47,11 +145,11 @@ public class WildcardPath {
 		this.replace(COUNTRY_WILDCARD, locale.getCountry());
 		return this;
 	}
-	
+
 	/**
 	 * Reset the path to initial state
 	 */
-	public void resetPath(){
+	public void resetPath() {
 		this.path = wildcardpath;
 		this.path = wildcardpath.replaceAll("\\.", "\\\\.");
 	}
@@ -92,6 +190,20 @@ public class WildcardPath {
 	}
 
 	/**
+	 * Si el path es de Windows, lo convierte al de unix / Es para poder evaluar
+	 * todos los path de la misma manera. Teóricamente no debe haber diferencias
+	 * 
+	 * @param filepath
+	 * @return
+	 */
+	private String escapedFilepath(String filepath) {
+		if (File.separator.equals("\\")) {
+			return filepath.replaceAll("\\\\", "/");
+		}
+		return filepath;
+	}
+
+	/**
 	 * This method return true if the file path match with the wildcard path
 	 * loaded into the WildcardPath object
 	 * 
@@ -105,6 +217,24 @@ public class WildcardPath {
 		Pattern p = Pattern.compile(path.getPath());
 		Matcher m = p.matcher(filepath);
 		return m.find();
+	}
+
+	/**
+	 * Extrae del filepath la parte que coincide con el wilcardpath declarado
+	 * 
+	 * @param filepath
+	 *            Path Completo al archivo de recursos
+	 * @return String con el la subseccion del filepath
+	 */
+	public String extractPath(String filepath) {
+		WildcardPath wp = new WildcardPath(wildcardpath);
+		wp.replaceToRegex();
+		Pattern p = Pattern.compile(wp.getPath());
+		Matcher m = p.matcher(filepath);
+		if (m.find()) {
+			return filepath.substring(m.start(), m.end());
+		}
+		return null;
 	}
 
 	public static void main(String[] args) {
@@ -121,5 +251,11 @@ public class WildcardPath {
 		System.out.println(wp1.match("src/locale/component.es_AR.properties"));
 		System.out.println(wp1.match("src/locale/component.en_US.properties"));
 		System.out.println(wp1.match("src/locale/component.es.properties"));
+		wp1
+				.parse("c:\\tempos\\TemposProject\\src\\localeProp\\component.en_US.properties");
+		System.out.println("Filename:" + wp1.getFileName());
+		System.out.println("Extension:" + wp1.getFileExtension());
+		System.out.println("Root: " + wp1.getRoot());
+		System.out.println("Locale: " + wp1.getLocale());
 	}
 }
