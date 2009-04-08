315e851ecadea184a5e26563a01e76ff03da2fe0
flores.leonardo <flores.leonardo@986cc4a0-e254-0410-b11a-59b74a07c1a1>
2009-04-08 10:39:53 +0000

    Actualizacion de loa documentacion

--- trunk/com.triadsoft.properties.editor/src/com/triadsoft/common/properties/PropertyEntry.java
+++ trunk/com.triadsoft.properties.editor/src/com/triadsoft/common/properties/PropertyEntry.java
@@ -6,116 +6,104 @@ package com.triadsoft.common.properties;
 import java.io.PrintWriter;
 
 /**
- * @author lflores
+ * @author Triad (flores.leonardo@triadsoft.com.ar)
  */
 public class PropertyEntry extends PropertyElement {
-    private String key;
-    private String value;
-    private int lineNumber;
-    private int start = 0;
-    private int end = 1;
+	private String key;
+	private String value;
+	private int lineNumber;
+	private int start = 0;
+	private int end = 1;
 
-    /**
-     * Crea la propiedad sin el control de la linea
-     * @param parent
-     * @param key
-     * @param value
-     */
-    public PropertyEntry(PropertyCategory parent, String key, String value) {
-        this(parent,key,value,0,2,0);
-    }
-    
-    public PropertyEntry(PropertyCategory parent, String key, String value,int start, int end,int lineNumber) {
-        super(parent);
-        this.key = key;
-        this.value = value;
-        this.lineNumber = lineNumber;
-        this.start = start;
-        this.end = end;
-    }
+	/**
+	 * Crea la propiedad sin el control de la linea
+	 * 
+	 * @param parent
+	 * @param key
+	 * @param value
+	 */
+	public PropertyEntry(PropertyCategory parent, String key, String value) {
+		this(parent, key, value, 0, 2, 0);
+	}
 
-    public String getKey()
-    {
-        return key;
-    }
+	public PropertyEntry(PropertyCategory parent, String key, String value,
+			int start, int end, int lineNumber) {
+		super(parent);
+		this.key = key;
+		this.value = value;
+		this.lineNumber = lineNumber;
+		this.start = start;
+		this.end = end;
+	}
 
-    public String getValue()
-    {
-        return value;
-    }
+	public String getKey() {
+		return key;
+	}
 
-    public PropertyElement[] getChildren()
-    {
-        return NO_CHILDREN;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public void setKey(String text)
-    {
-        if (key.equals(text))
-            return;
-        key = text;
-        ((PropertyCategory) getParent()).keyChanged(this);
-    }
+	public PropertyElement[] getChildren() {
+		return NO_CHILDREN;
+	}
 
-    public void setValue(String text)
-    {
-        if (value.equals(text))
-            return;
-        value = text;
-        ((PropertyCategory) getParent()).valueChanged(this);
-    }
+	public void setKey(String text) {
+		if (key.equals(text))
+			return;
+		key = text;
+		((PropertyCategory) getParent()).keyChanged(this);
+	}
 
-    public void removeFromParent()
-    {
-        ((PropertyCategory) getParent()).removeEntry(this);
-    }
+	public void setValue(String text) {
+		if (value.equals(text))
+			return;
+		value = text;
+		((PropertyCategory) getParent()).valueChanged(this);
+	}
 
-    public int getEnd()
-    {
-        return end;
-    }
+	public void removeFromParent() {
+		((PropertyCategory) getParent()).removeEntry(this);
+	}
 
-    public void setEnd(int end)
-    {
-        this.end = end;
-    }
+	public int getEnd() {
+		return end;
+	}
 
-    public int getStart()
-    {
-        return start;
-    }
+	public void setEnd(int end) {
+		this.end = end;
+	}
 
-    public void setStart(int start)
-    {
-        this.start = start;
-    }
+	public int getStart() {
+		return start;
+	}
 
-    /**
-     * @see com.sistran.tools.editors.views.PropertyElement#hasChildren()
-     */
-    public boolean hasChildren()
-    {
-        //the end of tree
-        return false;
-    }
+	public void setStart(int start) {
+		this.start = start;
+	}
 
-    public void appendText(PrintWriter writer)
-    {
-        writer.print(key);
-        writer.print("=");
-        writer.println(value);
-    }
+	/**
+	 * @see com.triadsoft.common.properties.PropertyElement#hasChildren()
+	 */
+	public boolean hasChildren() {
+		// the end of tree
+		return false;
+	}
 
-    public int getLine()
-    {
-       return this.lineNumber;
-    }
-    
-    /**
-     * @see java.lang.Object#toString()
-     */
-    public String toString()
-    {
-       return getKey()+"="+getValue();
-    }
+	public void appendText(PrintWriter writer) {
+		writer.print(key);
+		writer.print("=");
+		writer.println(value);
+	}
+
+	public int getLine() {
+		return this.lineNumber;
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	public String toString() {
+		return getKey() + "=" + getValue();
+	}
 }
