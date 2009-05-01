2a35d01b9b9a71d6876f398e6a53e461502827d5
flores.leonardo <flores.leonardo@986cc4a0-e254-0410-b11a-59b74a07c1a1>
2009-05-01 12:06:09 +0000

    Wizard para crear los archivos de recursos

--- com.triadsoft.properties.editor/src/com/triadsoft/properties/wizards/LocalizedProperties.java
+++ com.triadsoft.properties.editor/src/com/triadsoft/properties/wizards/LocalizedProperties.java
@@ -12,18 +12,17 @@ import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.CoreException;
 import java.io.*;
+
 import org.eclipse.ui.*;
 import org.eclipse.ui.ide.IDE;
 
 /**
- * This is a sample new wizard. Its role is to create a new file 
- * resource in the provided container. If the container resource
- * (a folder or a project) is selected in the workspace 
- * when the wizard is opened, it will accept it as the target
- * container. The wizard creates one file with the extension
- * "properties". If a sample multi-page editor (also available
- * as a template) is registered for the same extension, it will
- * be able to open it.
+ * This is a sample new wizard. Its role is to create a new file resource in the
+ * provided container. If the container resource (a folder or a project) is
+ * selected in the workspace when the wizard is opened, it will accept it as the
+ * target container. The wizard creates one file with the extension
+ * "properties". If a sample multi-page editor (also available as a template) is
+ * registered for the same extension, it will be able to open it.
  */
 
 public class LocalizedProperties extends Wizard implements INewWizard {
@@ -37,7 +36,7 @@ public class LocalizedProperties extends Wizard implements INewWizard {
 		super();
 		setNeedsProgressMonitor(true);
 	}
-	
+
 	/**
 	 * Adding the page to the wizard.
 	 */
@@ -48,17 +47,19 @@ public class LocalizedProperties extends Wizard implements INewWizard {
 	}
 
 	/**
-	 * This method is called when 'Finish' button is pressed in
-	 * the wizard. We will create an operation and run it
-	 * using wizard as execution context.
+	 * This method is called when 'Finish' button is pressed in the wizard. We
+	 * will create an operation and run it using wizard as execution context.
 	 */
 	public boolean performFinish() {
+		final String filepath = page.getFilePath().substring(0,
+				page.getFilePath().lastIndexOf("/"));
 		final String containerName = page.getContainerName();
 		final String fileName = page.getFileName();
 		IRunnableWithProgress op = new IRunnableWithProgress() {
-			public void run(IProgressMonitor monitor) throws InvocationTargetException {
+			public void run(IProgressMonitor monitor)
+					throws InvocationTargetException {
 				try {
-					doFinish(containerName, fileName, monitor);
+					doFinish(containerName, filepath, fileName, monitor);
 				} catch (CoreException e) {
 					throw new InvocationTargetException(e);
 				} finally {
@@ -72,32 +73,39 @@ public class LocalizedProperties extends Wizard implements INewWizard {
 			return false;
 		} catch (InvocationTargetException e) {
 			Throwable realException = e.getTargetException();
-			MessageDialog.openError(getShell(), "Error", realException.getMessage());
+			MessageDialog.openError(getShell(), "Error", realException
+					.getMessage());
 			return false;
 		}
 		return true;
 	}
-	
+
 	/**
-	 * The worker method. It will find the container, create the
-	 * file if missing or just replace its contents, and open
-	 * the editor on the newly created file.
+	 * The worker method. It will find the container, create the file if missing
+	 * or just replace its contents, and open the editor on the newly created
+	 * file.
 	 */
 
-	private void doFinish(
-		String containerName,
-		String fileName,
-		IProgressMonitor monitor)
-		throws CoreException {
+	private void doFinish(String containerName, String filepath,
+			String fileName, IProgressMonitor monitor) throws CoreException {
 		// create a sample file
 		monitor.beginTask("Creating " + fileName, 2);
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
-		IResource resource = root.findMember(new Path(containerName));
-		if (!resource.exists() || !(resource instanceof IContainer)) {
-			throwCoreException("Container \"" + containerName + "\" does not exist.");
+		IResource resource = root
+				.findMember(new Path(containerName + filepath));
+		IResource containerFolder = root.findMember(new Path(containerName));
+		if (resource == null || !resource.exists()
+				|| !(resource instanceof IContainer)) {
+			IFolder folder = ((IContainer) containerFolder).getFolder(new Path(
+					filepath));
+			folder.create(false, false, monitor);
+			resource = root.findMember(new Path(containerName + filepath));
+			// throwCoreException("Container \"" + containerName +
+			// "\" does not exist.");
 		}
 		IContainer container = (IContainer) resource;
-		final IFile file = container.getFile(new Path(fileName));
+		final IFile file = container
+				.getFile(new Path(fileName + ".properties"));
 		try {
 			InputStream stream = openContentStream();
 			if (file.exists()) {
@@ -112,36 +120,36 @@ public class LocalizedProperties extends Wizard implements INewWizard {
 		monitor.setTaskName("Opening file for editing...");
 		getShell().getDisplay().asyncExec(new Runnable() {
 			public void run() {
-				IWorkbenchPage page =
-					PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+				IWorkbenchPage page = PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow().getActivePage();
 				try {
-					IDE.openEditor(page, file, true);
+					IDE.openEditor(page, file, "com.triadsoft.properties.editors.PropertiesEditor");
 				} catch (PartInitException e) {
 				}
 			}
 		});
 		monitor.worked(1);
 	}
-	
+
 	/**
 	 * We will initialize file contents with a sample text.
 	 */
 
 	private InputStream openContentStream() {
-		String contents =
-			"This is the initial file contents for *.properties file that should be word-sorted in the Preview page of the multi-page editor";
+		String contents = "This is the initial file contents for *.properties file that should be word-sorted in the Preview page of the multi-page editor";
 		return new ByteArrayInputStream(contents.getBytes());
 	}
 
 	private void throwCoreException(String message) throws CoreException {
-		IStatus status =
-			new Status(IStatus.ERROR, "com.triadsoft.properties", IStatus.OK, message, null);
+		IStatus status = new Status(IStatus.ERROR, "com.triadsoft.properties",
+				IStatus.OK, message, null);
 		throw new CoreException(status);
 	}
 
 	/**
-	 * We will accept the selection in the workbench to see if
-	 * we can initialize from it.
+	 * We will accept the selection in the workbench to see if we can initialize
+	 * from it.
+	 * 
 	 * @see IWorkbenchWizard#init(IWorkbench, IStructuredSelection)
 	 */
 	public void init(IWorkbench workbench, IStructuredSelection selection) {
