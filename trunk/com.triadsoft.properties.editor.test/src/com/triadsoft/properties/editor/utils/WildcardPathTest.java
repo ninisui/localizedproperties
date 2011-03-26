d8c5006c034678bf6f0cd51c91946339f7469aad
flores.leonardo <flores.leonardo@986cc4a0-e254-0410-b11a-59b74a07c1a1>
2011-03-26 12:19:16 +0000

    Nueva version de WildcardPath

--- trunk/com.triadsoft.properties.editor.test/src/com/triadsoft/properties/editor/utils/WildcardPathTest.java
+++ trunk/com.triadsoft.properties.editor.test/src/com/triadsoft/properties/editor/utils/WildcardPathTest.java
@@ -1,16 +1,19 @@
 package com.triadsoft.properties.editor.utils;
 
-import java.util.Locale;
 import java.util.StringTokenizer;
 
 import junit.framework.TestCase;
 
 import org.junit.Test;
 
+import com.triadsoft.properties.model.utils.IWildcardPath;
+import com.triadsoft.properties.model.utils.WildCardPath2;
 import com.triadsoft.properties.model.utils.WildcardPath;
 
 /**
- * Prueba
+ * Prueba de la nueva version del WildCardPath Este test hace pruebas unitarias
+ * sobre los wildcardpath Falta hacer una test de integracion para hacer las
+ * pruebas cuando existe mas de uno
  * 
  * @author Leonardo Flores()
  * 
@@ -18,12 +21,198 @@ import com.triadsoft.properties.model.utils.WildcardPath;
 public class WildcardPathTest extends TestCase {
 	private static final String FLEX_PROPERTIES = "/{root}/{lang}_{country}/{filename}.{fileextension}";
 	private static final String JAVA_PROPERTIES = "/{root}/{filename}.{lang}_{country}.{fileextension}";
+	private static final String WEB_PROPERTIES = "/{root}/{filename}.{lang}_{country}.{fileextension}";
 	private static final String JOOMLA_INI = "/{root}/{lang}-{country}/{lang}-{country}.{filename}.{fileextension}";
+	private static final String JAVA_PROPERTIES_WITHOUT_ROOT = "{filename}.{lang}_{country}.{fileextension}";
+
+	@Test
+	public void testJava() {
+		WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES);
+		String path = "c:\\tempos\\TemposProject\\src\\locale\\prueba\\component.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es component",
+				"component".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root no es prueba",
+				"prueba".equals(wp.getRoot()));
+	}
+
+	@Test
+	public void testJavaConNumero() {
+		WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES);
+		String path = "c:\\tempos\\TemposProject\\src\\locale\\prueba\\component2.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es component2",
+				"component2".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root no es prueba",
+				"prueba".equals(wp.getRoot()));
+	}
+
+	@Test
+	public void testJavaConNumeroUnix() {
+		WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES);
+		String path = "/tempos/TemposProject/src/locale/prueba/component2.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es component2",
+				"componnt2".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root no es prueba",
+				"prueba".equals(wp.getRoot()));
+	}
+
+	@Test
+	public void testJavaDefault() {
+		WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES);
+		String path = "c:\\tempos\\TemposProject\\src\\locale\\prueba\\component.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es component",
+				"component".equals(wp.getFileName()));
+		assertTrue("No debe tener lenguaje", wp.getLanguage() == null);
+		assertTrue("No debe tener pais", wp.getCountry() == null);
+		assertTrue("La carpeta root no es prueba",
+				"prueba".equals(wp.getRoot()));
+	}
+
+	@Test
+	public void testJavaWithUnderscore() {
+		WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES);
+		String path = "c:\\tempos\\TemposProject\\src\\locale\\prueba\\core_component.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es core_component",
+				"core_component".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root no es prueba",
+				"prueba".equals(wp.getRoot()));
+	}
+
+	// @Test
+	// public void testReplacement() {
+	// WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES);
+	// wp.replace(IWildcardPath.ROOT_WILDCARD, "locale")
+	// .replace(IWildcardPath.FILENAME_WILDCARD, "alert")
+	// .replace(IWildcardPath.FILE_EXTENSION_WILDCARD, "properties");
+	// wp.replace(Locale.getDefault());
+	// }
+
+	@Test
+	public void testJavaWithoutCountry() {
+		IWildcardPath wp = new WildCardPath2(JAVA_PROPERTIES);
+		String path = "c:\\tempos\\TemposProject\\src\\locale\\prueba\\component.en.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+		wp.parse(path);
+		assertTrue("El nombre del archivo debe ser component",
+				"component".equals(wp.getFileName()));
+		assertTrue("El lenguaje deberia ser en", "en".equals(wp.getLanguage()));
+		assertTrue("El country deberia ser null", wp.getCountry() == null);
+	}
+
+	@Test
+	public void testJavaWithoutCountryAndLanguage() {
+		IWildcardPath wp = new WildCardPath2(JAVA_PROPERTIES);
+		String path = "c:\\tempos\\TemposProject\\src\\locale\\prueba\\component.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+		wp.parse(path);
+		assertTrue("El nombre del archivo debe ser component",
+				"component".equals(wp.getFileName()));
+		assertTrue("El lenguaje deberia ser null", wp.getLanguage() == null);
+		assertTrue("El country deberia ser null", wp.getCountry() == null);
+	}
+
+	@Test
+	public void testFileTokenizer() {
+		String text = "L/test_ini_files/language/en-GB/en-GB.com_frontpage.ini";
+		StringTokenizer tokenizer = new StringTokenizer(text, "/");
+		assertTrue("Encontre 5 elemnetos", tokenizer.countTokens() == 5);
+		tokenizer = new StringTokenizer(text, "/.-_");
+		System.out.println("encontre " + tokenizer.countTokens());
+		assertTrue("Encontre 12 elementos", tokenizer.countTokens() == 12);
+
+		while (tokenizer.hasMoreElements()) {
+			System.out.println(tokenizer.nextElement());
+		}
+	}
+
+	@Test
+	public void testJoomlaLanguageIniFiles() {
+		WildcardPath wp = new WildcardPath(JOOMLA_INI);
+		String filepath = "c:\\tempos\\test_ini_files\\language\\en-GB\\en-GB.com_content.ini";
+		assertTrue("El wildcard no coincide", wp.match(filepath));
+		wp.parse(filepath);
+		assertTrue("El lenguaje tiene que ser 'en'",
+				"en".equals(wp.getLanguage()));
+		assertTrue("El pais tiene que ser 'GB'", "GB".equals(wp.getCountry()));
+		assertTrue("El nombre del archivo no es com_content",
+				"com_content".equals(wp.getFileName()));
+		assertTrue("La extension del archivo no es ini",
+				"ini".equals(wp.getFileExtension()));
+		assertTrue("El root no es language", "language".equals(wp.getRoot()));
+	}
+
+	@Test
+	public void testJoomlaLanguageIniFilesUnix() {
+		WildcardPath wp = new WildcardPath(JOOMLA_INI);
+		String filepath = "/tempos/test_ini_files/language/en-GB/en-GB.com_content.ini";
+		assertTrue("El wildcard no coincide", wp.match(filepath));
+		wp.parse(filepath);
+		assertTrue("El lenguaje tiene que ser 'en'",
+				"en".equals(wp.getLanguage()));
+		assertTrue("El pais tiene que ser 'GB'", "GB".equals(wp.getCountry()));
+		assertTrue("El nombre del archivo no es com_content",
+				"com_content".equals(wp.getFileName()));
+		assertTrue("La extension del archivo no es ini",
+				"ini".equals(wp.getFileExtension()));
+		assertTrue("El root no es language", "language".equals(wp.getRoot()));
+	}
+
+	// @Test
+	// public void testJoomlaJavaMatchFiles() {
+	// IWildcardPath joowp = new WildcardPath(JOOMLA_INI);
+	// IWildcardPath jawp = new WildcardPath(JAVA_PROPERTIES);
+	// assertTrue(
+	// "El wildcard no coincide",
+	// joowp.match("c:\\tempos\\test_ini_files\\language\\en-GB\\en-GB.com_content.ini"));
+	//
+	// assertTrue(
+	// "El wildcard de java no debe coincidir",
+	// jawp.match("c:\\tempos\\test_ini_files\\language\\en-GB\\en-GB.com_content.ini"));
+	//
+	// }
 
 	@Test
 	public void testFlex() {
 		WildcardPath wp = new WildcardPath(FLEX_PROPERTIES);
-		wp.parse("c:\\tempos\\TemposProject\\src\\locale\\en_US\\component.properties");
+		String filepath = "c:\\tempos\\TemposProject\\src\\locale\\en_US\\component.properties";
+		assertTrue("Deberia coincidir con el properties de flex",
+				wp.match(filepath));
+		wp.parse(filepath);
+		assertTrue("El nombre del archivo no es component",
+				"component".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root no es locale",
+				"locale".equals(wp.getRoot()));
+	}
+
+	@Test
+	public void testFlexUnixFile() {
+		WildcardPath wp = new WildcardPath(FLEX_PROPERTIES);
+		String filepath = "/tempos/TemposProject/src/locale/en_US/component.properties";
+		assertTrue("Deberia coincidir con el properties de flex",
+				wp.match(filepath));
+		wp.parse(filepath);
 		assertTrue("El nombre del archivo no es component",
 				"component".equals(wp.getFileName()));
 		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
@@ -45,105 +234,143 @@ public class WildcardPathTest extends TestCase {
 	}
 
 	@Test
-	public void testJava() {
-		WildcardPath wp = new WildcardPath(JAVA_PROPERTIES);
-		wp.parse("c:\\tempos\\TemposProject\\src\\locale\\prueba\\component.en_US.properties");
+	public void testWebInf() {
+		WildCardPath2 wp = new WildCardPath2(WEB_PROPERTIES);
+		String path = "c:\\tempos\\TemposProject\\src\\web\\WEB-INF\\component.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+
+		wp.parse(path);
 		assertTrue("El nombre del archivo no es component",
 				"component".equals(wp.getFileName()));
 		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
 		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
-		assertTrue("La carpeta root no es prueba",
-				"prueba".equals(wp.getRoot()));
+		assertTrue("La carpeta root no es WEB-INF",
+				"WEB-INF".equals(wp.getRoot()));
 	}
 
 	@Test
-	public void testJavaDefault() {
-		WildcardPath wp = new WildcardPath(JAVA_PROPERTIES);
-		wp.parse("c:\\tempos\\TemposProject\\src\\locale\\prueba\\component.properties");
+	public void testWebInfConNumero() {
+		WildCardPath2 wp = new WildCardPath2(WEB_PROPERTIES);
+		String path = "c:\\tempos\\TemposProject\\src\\web\\WEB-INF\\component2.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es component2",
+				"component2".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root no es WEB-INF",
+				"WEB-INF".equals(wp.getRoot()));
+	}
+
+	@Test
+	public void testWebInfConNumeroUnix() {
+		WildCardPath2 wp = new WildCardPath2(WEB_PROPERTIES);
+		String path = "/tempos/TemposProject/src/web/WEB-INF/component2.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es component2",
+				"component2".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root no es WEB-INF",
+				"WEB-INF".equals(wp.getRoot()));
+	}
+
+	@Test
+	public void testWebInfDefault() {
+		WildCardPath2 wp = new WildCardPath2(WEB_PROPERTIES);
+		String path = "c:\\tempos\\TemposProject\\src\\web\\WEB-INF\\component.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+		wp.parse(path);
 		assertTrue("El nombre del archivo no es component",
 				"component".equals(wp.getFileName()));
 		assertTrue("No debe tener lenguaje", wp.getLanguage() == null);
 		assertTrue("No debe tener pais", wp.getCountry() == null);
-		assertTrue("La carpeta root no es prueba",
-				"prueba".equals(wp.getRoot()));
+		assertTrue("La carpeta root no es WEB-INF",
+				"WEB-INF".equals(wp.getRoot()));
 	}
 
 	@Test
-	public void testJavaWithUnderscore() {
-		WildcardPath wp = new WildcardPath(JAVA_PROPERTIES);
-		wp.parse("c:\\tempos\\TemposProject\\src\\locale\\prueba\\core_component.en_US.properties");
+	public void testWebInfWithUnderscore() {
+		WildCardPath2 wp = new WildCardPath2(WEB_PROPERTIES);
+		String path = "c:\\tempos\\TemposProject\\src\\web\\WEB-INF\\core_component.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+		wp.parse(path);
 		assertTrue("El nombre del archivo no es core_component",
 				"core_component".equals(wp.getFileName()));
 		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
 		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
-		assertTrue("La carpeta root no es prueba",
-				"prueba".equals(wp.getRoot()));
+		assertTrue("La carpeta root no es WEB-INF",
+				"WEB-INF".equals(wp.getRoot()));
 	}
 
 	@Test
-	public void testReplacement() {
-		WildcardPath wp = new WildcardPath(JAVA_PROPERTIES);
-		wp.replace(WildcardPath.ROOT_WILDCARD, "locale")
-				.replace(WildcardPath.FILENAME_WILDCARD, "alert")
-				.replace(WildcardPath.FILE_EXTENSION_WILDCARD, "properties");
-		wp.replace(Locale.getDefault());
+	public void testJavaWithoutRoot() {
+		WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES_WITHOUT_ROOT);
+		String path = "c:\\tempos\\TemposProject\\src\\locale\\prueba\\component.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es component",
+				"component".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root debe ser null", wp.getRoot() == null);
 	}
 
 	@Test
-	public void testJavaWithoutCountry() {
-		WildcardPath wp = new WildcardPath(JAVA_PROPERTIES);
-		wp.parse("c:\\tempos\\TemposProject\\src\\locale\\prueba\\core_component.en.properties");
-		assertTrue("No pude encontrar el nombre del archivo",
-				wp.getFileName() != null);
-		assertTrue("No pude encontrar el lenguaje", wp.getLanguage() != null);
+	public void testJavaConNumeroWithoutRoot() {
+		WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES_WITHOUT_ROOT);
+		String path = "c:\\tempos\\TemposProject\\src\\locale\\prueba\\component2.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es component2",
+				"component2".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root debe ser null", wp.getRoot() == null);
 	}
 
 	@Test
-	public void testJoomlaLanguageIniFiles() {
-		WildcardPath wp = new WildcardPath(JOOMLA_INI);
-		assertTrue(
-				"El wildcard no coincide",
-				wp.match("c:\\tempos\\test_ini_files\\language\\en-GB\\en-GB.com_content.ini"));
-		wp.parse("c:\\tempos\\test_ini_files\\language\\en-GB\\en-GB.com_content.ini");
-		assertTrue("No encontré el lenguage", wp.getLanguage() != null);
-		assertTrue("No encontré el pais", wp.getCountry() != null);
-		assertTrue("El lenguaje no es ingles", wp.getLanguage().equals("en"));
-		assertTrue("El pais no es Gran Bretaña", wp.getCountry().equals("GB"));
-		assertTrue("El nombre del archivo no es com_content", wp.getFileName()
-				.equals("com_content"));
-		assertTrue("La extension del archivo no es ini", wp.getFileExtension()
-				.equals("ini"));
-		assertTrue("El root no es language", wp.getRoot().equals("language"));
+	public void testJavaConNumeroUnixWithoutRoot() {
+		WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES_WITHOUT_ROOT);
+		String path = "/tempos/TemposProject/src/locale/prueba/component2.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
 
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es component2",
+				"component2".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root debe ser null", wp.getRoot() == null);
 	}
 
 	@Test
-	public void testJoomlaJavaMatchFiles() {
-		WildcardPath joowp = new WildcardPath(JOOMLA_INI);
-		WildcardPath jawp = new WildcardPath(JAVA_PROPERTIES);
-		assertTrue(
-				"El wildcard no coincide",
-				joowp.match("c:\\tempos\\test_ini_files\\language\\en-GB\\en-GB.com_content.ini"));
-
-		assertTrue(
-				"El wildcard de java no debe coincidir",
-				jawp.match("c:\\tempos\\test_ini_files\\language\\en-GB\\en-GB.com_content.ini"));
-
+	public void testJavaDefaultWithoutRoot() {
+		WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES_WITHOUT_ROOT);
+		String path = "c:\\tempos\\TemposProject\\src\\locale\\prueba\\component.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es component",
+				"component".equals(wp.getFileName()));
+		assertTrue("No debe tener lenguaje", wp.getLanguage() == null);
+		assertTrue("No debe tener pais", wp.getCountry() == null);
+		assertTrue("La carpeta root debe ser null", wp.getRoot() == null);
 	}
 
 	@Test
-	public void testFileTokenizer() {
-		String text = "L/test_ini_files/language/en-GB/en-GB.com_frontpage.ini";
-		StringTokenizer tokenizer = new StringTokenizer(text, "/");
-		assertTrue("Encontre 5 elemnetos", tokenizer.countTokens() == 5);
-		tokenizer = new StringTokenizer(text, "/.-_");
-		System.out.println("encontre " + tokenizer.countTokens());
-		assertTrue("Encontre 12 elementos", tokenizer.countTokens() == 12);
-		
-		while (tokenizer.hasMoreElements()) {
-			System.out.println(tokenizer.nextElement());
-		}
-		
-		
+	public void testJavaWithUnderscoreWithoutRoot() {
+		WildCardPath2 wp = new WildCardPath2(JAVA_PROPERTIES_WITHOUT_ROOT);
+		String path = "c:\\tempos\\TemposProject\\src\\locale\\prueba\\core_component.en_US.properties";
+		assertTrue("El path debería coincidir", wp.match(path));
+		wp.parse(path);
+		assertTrue("El nombre del archivo no es core_component",
+				"core_component".equals(wp.getFileName()));
+		assertTrue("El lenguaje no es en", "en".equals(wp.getLanguage()));
+		assertTrue("El pais no es US", "US".equals(wp.getCountry()));
+		assertTrue("La carpeta root no es prueba", wp.getRoot() == null);
 	}
 }
