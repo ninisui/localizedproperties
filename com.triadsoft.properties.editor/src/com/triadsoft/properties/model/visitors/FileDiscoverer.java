62ba0cd9605a29c40d5a9802a66291b1dda36abd
flores.leonardo <flores.leonardo@986cc4a0-e254-0410-b11a-59b74a07c1a1>
2011-04-09 18:30:53 +0000

    Refactoring del wilcard path

---
+++ com.triadsoft.properties.editor/src/com/triadsoft/properties/model/visitors/FileDiscoverer.java
@@ -0,0 +1,66 @@
+package com.triadsoft.properties.model.visitors;
+
+import java.util.List;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.runtime.CoreException;
+
+import com.triadsoft.properties.model.utils.IWildcardPath;
+import com.triadsoft.properties.model.utils.WildCardPath2;
+
+/**
+ * Este file visitor, intenta descubrir los archivos que tienen el mismo nombre
+ * y extension que el del wildcard path. De ésta manera se descubren archivos
+ * candidatos a matchear con el wp
+ * 
+ * @author Triad (flores.leonardo@gmail.com)
+ */
+public class FileDiscoverer implements IResourceVisitor {
+	private IWildcardPath wp;
+	private String filename;
+	private String extension;
+	private String root;
+	private int offset;
+
+	private List<IFile> files;
+
+	public FileDiscoverer(List<IFile> files, WildCardPath2 wp, String filename,
+			String extension, String root, int offset) {
+		this.files = files;
+		this.root = root;
+		this.wp = wp;
+		this.filename = filename;
+		this.extension = extension;
+		this.offset = offset;
+	}
+
+	public boolean visit(IResource resource) throws CoreException {
+		String filepath = resource.getFullPath().toFile().getAbsolutePath();
+		if (resource.getType() == IResource.FOLDER && root != null
+				&& resource.getName().equals(root)) {
+			return true;
+		} else if (resource.getType() == IResource.FOLDER && root != null
+				&& !resource.getName().equals(root)) {
+			return false;
+		} else if (resource.getType() == IResource.FOLDER && root == null) {
+			return true;
+		} else if (resource.getType() == IResource.FILE
+				&& wp.match(filepath, offset)) {
+			// Si el archivo coincide con el wp, entonces lo parseo
+			// para ver si el wp me devuelve el filename y extension esperado
+			wp.parse(filepath, offset);
+			if (wp.getFileName() != null && wp.getFileName().equals(filename)
+					&& wp.getFileExtension() != null
+					&& wp.getFileExtension().equals(extension)) {
+				files.add((IFile) resource);
+				return true;
+			}
+			// sino coinciden entonces no es el archivo correcto
+		} else if (resource.getType() == IResource.PROJECT) {
+			return true;
+		}
+		return false;
+	}
+}
---
+++ com.triadsoft.properties.editor/src/com/triadsoft/properties/model/visitors/FileVisitor.java
@@ -0,0 +1,77 @@
+package com.triadsoft.properties.model.visitors;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.runtime.CoreException;
+
+import com.triadsoft.properties.model.utils.IWildcardPath;
+import com.triadsoft.properties.model.utils.StringUtils;
+import com.triadsoft.properties.model.utils.WildcardPath;
+
+/**
+ * Clase Visitor que se encarga de obtener los archivos que cumplen con el
+ * nombre y la ubicacion del recurso consultado en el metodo visit
+ * 
+ * @author Triad (flores.leonardo@gmail.com)
+ * @deprecated No se debe usar mas, se debe reemplazar por FileDiscoverer
+ */
+public class FileVisitor implements IResourceVisitor {
+
+	private Map<Locale, IFile> locales;
+	private IWildcardPath wp;
+	private IWildcardPath comparator;
+
+	public FileVisitor(Map<Locale, IFile> locales, IWildcardPath wp) {
+		this.locales = locales;
+		this.wp = wp;
+		this.comparator = new WildcardPath(wp.getWildcardpath());
+	}
+
+	/**
+	 * Metodo que revisa el nombre y la ubicacion del recurso
+	 * 
+	 * @param resource
+	 *            Recurso que ser� evaluado
+	 * @return True si el archivo puede ser evaluado o FALSE en caso que no se
+	 *         deba revisar. El unico caso en que se devuelve false es cuando el
+	 *         recurso es un archivo y no cumple con el nombre esperado.En caso
+	 *         que sea una carpeta devuelve true para que puede evaluar los
+	 *         archivos internos.
+	 */
+	public boolean visit(IResource resource) throws CoreException {
+		// Pattern p = Pattern.compile(wp.getLocaleRegex());
+		// Matcher m = p.matcher(resource.getFullPath().toString());
+
+		if (resource.getType() == IFile.FOLDER) {
+			return true;
+		} else if (resource.getType() == IFile.PROJECT) {
+			return true;
+		} else if (resource.getType() == IFile.FILE) {
+			IFile file = (IFile) resource;
+			comparator.parse(file.getFullPath().toString());
+			boolean isFilename = wp.getFileName().equals(
+					comparator.getFileName());
+			boolean isFileExtension = wp.getFileExtension().equals(
+					comparator.getFileExtension());
+
+			if (isFilename && isFileExtension) {
+				comparator.resetPath();
+				if (comparator.parse(resource.getFullPath().toString())) {
+					if (comparator.getLocale() == null) {
+						locales.put(StringUtils.getKeyLocale(),
+								(IFile) resource);
+					} else {
+						locales.put(comparator.getLocale(), (IFile) resource);
+					}
+					return true;
+				}
+			}
+			return false;
+		}
+		return false;
+	}
+}
