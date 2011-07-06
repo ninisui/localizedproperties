fe56e8ade2f01331a61d1f9a780b93066213d829
flores.leonardo <flores.leonardo@986cc4a0-e254-0410-b11a-59b74a07c1a1>
2011-07-06 02:31:00 +0000

    http://code.google.com/p/localizedproperties/issues/detail?id=81 First implementation of i/e extension point

--- trunk/com.triadsoft.properties.editor/src/com/triadsoft/properties/editor/extensions/IPropertiesImport.java
+++ trunk/com.triadsoft.properties.editor/src/com/triadsoft/properties/editor/extensions/IPropertiesImport.java
@@ -6,37 +6,17 @@ import com.triadsoft.properties.model.Property;
  * This is an interface to modelate an import extension point
  * 
  * @author Triad (flores.leonardo@gmail.com)
+ * @since 0.8.0
  */
-public interface IPropertiesImport {
-	/**
-	 * This is the friendly name to the extension. Ej: Import to Excel
-	 * 
-	 * @return
-	 */
-	public String getName();
-
-	/**
-	 * This method return a description to the extension. Ej: Import all
-	 * properties to an Excel File
-	 * 
-	 * @return
-	 */
-	public String getDescription();
-
+public interface IPropertiesImport extends IPropertiesIE {
 	/**
 	 * This method receive from extension an array of property objects to import
 	 * to editor
-	 * @param properties Array of Property object
+	 * 
+	 * @param properties
+	 *            Array of Property object
 	 * @return
 	 * @see Property
 	 */
 	public Property[] importProperties();
-
-	/**
-	 * This text will be show in contextual menu to call the implementation of
-	 * action
-	 * 
-	 * @return String Localized Text
-	 */
-	public String getMenuName();
 }
