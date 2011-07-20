015fc23182a463bd4dba9fbe27de64445e69484f
flores.leonardo <flores.leonardo@986cc4a0-e254-0410-b11a-59b74a07c1a1>
2011-07-20 19:53:41 +0000

    Issue 83 - It must be implemented variant of Locale http://code.google.com/p/localizedproperties/issues/detail?id=83 Last changes and testing. I solved distincts problems about use and configuration of Variant. Now It's supported just for Java Full path

--- com.triadsoft.properties.editor.test/src/com/triadsoft/properties/editor/plugin/DefaultsPreferencesTest.java
+++ com.triadsoft.properties.editor.test/src/com/triadsoft/properties/editor/plugin/DefaultsPreferencesTest.java
@@ -6,6 +6,16 @@ import com.triadsoft.properties.editor.LocalizedPropertiesPlugin;
 
 public class DefaultsPreferencesTest extends TestCase {
 
+	public static final String FLEX_WILDCARD = "/{root}/{lang}_{country}/{filename}.{fileextension}";
+	public static final String LANG_DOT_COUNTRY_WILDCARD = "/{root}/{lang}.{country}/{filename}.{fileextension}";
+	public static final String JOOMLA_WILDCARD = "/{root}/{lang}-{country}/{lang}-{country}.{filename}.{fileextension}";
+	public static final String LANG_SLASH_COUNTRY_FILENAME_WILDCARD = "/{root}/{lang}/{country}/{filename}.{fileextension}";
+	public static final String JAVA_FULL = "/{root}/{filename}(_{lang})3(_{country})2(_{variant})1.{fileextension}";
+	public static final String JAVA_DOUBLE_DOT_WILDCARD = "/{root}/{filename}(.{lang})2(.{country})1.{fileextension}";
+	public static final String JAVA_DOT_WILDCARD = "/{root}/{filename}(.{lang})2(_{country})1.{fileextension}";
+	public static final String ALEJANDRA_WILDCARD = "/{root}/{filename}(_{lang})2(_{country})1.{fileextension}";
+	
+
 	public void setUp() throws Exception {
 		super.setUp();
 	}
@@ -17,26 +27,27 @@ public class DefaultsPreferencesTest extends TestCase {
 	public void testWildcardDefaults() {
 		String[] wps = LocalizedPropertiesPlugin.getWildcardPaths();
 		assertTrue("No se encontro el wp para flex",
-				"/{root}/{lang}_{country}/{filename}.{fileextension}"
+				FLEX_WILDCARD
 						.equals(wps[0]));
-		assertTrue("No se encontro el wp para java completo",
-				"/{root}/{filename}(.{lang})2(_{country})1.{fileextension}"
-						.equals(wps[1]));
 		assertTrue("No se encontro el wp para java con punto",
-				"/{root}/{filename}(.{lang})2(.{country})1.{fileextension}"
+				JAVA_DOT_WILDCARD
+						.equals(wps[1]));
+		assertTrue("No se encontro el wp para java con doble punto",
+				JAVA_DOUBLE_DOT_WILDCARD
 						.equals(wps[2]));
 		assertTrue("No se encontro el directorio lang.country",
-				"/{root}/{lang}.{country}/{filename}.{fileextension}"
+				LANG_DOT_COUNTRY_WILDCARD
 						.equals(wps[3]));
 		assertTrue("No se encontro el wp para el directorio lang/country/filename.fileextension",
-				"/{root}/{lang}/{country}/{filename}.{fileextension}"
+				LANG_SLASH_COUNTRY_FILENAME_WILDCARD
 						.equals(wps[4]));
 		assertTrue("No se encontro el wp para una variante de Java lang_country",
-				"/{root}/{filename}(_{lang})2(_{country})1.{fileextension}"
+				JAVA_FULL
 						.equals(wps[5]));
 		assertTrue("No se encontro el wp para Joomla",
-				"/{root}/{lang}-{country}/{lang}-{country}.{filename}.{fileextension}".equals(wps[6]));
-		assertTrue("No se encontro el wp para filename.country.extension",
-				"/{root}/{filename}.{country}.{fileextension}".equals(wps[7]));
+				JOOMLA_WILDCARD.equals(wps[6]));
+		//It's a simplification of JAVA_DOT_WILDCARD
+//		assertTrue("No se encontro el wp para filename.country.extension",
+//				SIMPLE_WITH_COUNTRY_WILDCARD.equals(wps[7]));
 	}
 }
