8b8040f64113519dbe2ec72a23326b0aaeeb8044
flores.leonardo <flores.leonardo@gmail.com>
2012-11-03 13:23:03 +0000

    0.8.5 Release Candidate Package export updates

--- com.triadsoft.properties.editor.test/src/com/triadsoft/properties/NewPropertiesTest.java
+++ com.triadsoft.properties.editor.test/src/com/triadsoft/properties/NewPropertiesTest.java
@@ -1,11 +1,13 @@
 package com.triadsoft.properties;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import junit.framework.TestCase;
 
+import org.eclipse.core.runtime.CoreException;
 import org.junit.Test;
 
 import com.triadsoft.properties.model.PropertiesFile;
@@ -90,28 +92,36 @@ public class NewPropertiesTest extends TestCase {
 		assertTrue("No se encontraron todas las claves esperadas", props
 				.keySet().size() == 678);
 	}
-	
+
 	@Test
 	public void testLanguage_es_AR_Properties() {
 		assertTrue("No se pudo cargar el archivo", simpleProperties != null);
 		PropertiesFile props = new PropertiesFile();
-		try {
-			InputStream es_AR=new FileInputStream(PATH + "language.es_AR.properties");
-			props.load(es_AR);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		File es_AR = new File(PATH + "language.es_AR.properties");
+		props = new PropertiesFile(es_AR);
+		
 		System.out.println(props.keySet().size());
 		assertTrue("No se encontraron todas las claves esperadas", props
 				.keySet().size() == 678);
+		String name = props.getProperty("label.form.recoveryPassword");
+		System.out.println(name);
+		props.setProperty("mi.nueva.clave", "És es un prueba");
+		try {
+			props.save();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (CoreException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	@Test
 	public void testLanguage_en_US_Properties() {
 		assertTrue("No se pudo cargar el archivo", simpleProperties != null);
 		PropertiesFile props = new PropertiesFile();
 		try {
-			InputStream en_US=new FileInputStream(PATH + "language.en_US.properties");
+			InputStream en_US = new FileInputStream(PATH
+					+ "language.en_US.properties");
 			props.load(en_US);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -120,13 +130,14 @@ public class NewPropertiesTest extends TestCase {
 		assertTrue("No se encontraron todas las claves esperadas", props
 				.keySet().size() == 678);
 	}
-	
+
 	@Test
 	public void testLanguage_short_Properties() {
 		assertTrue("No se pudo cargar el archivo", simpleProperties != null);
 		PropertiesFile props = new PropertiesFile();
 		try {
-			InputStream en_US=new FileInputStream(PATH + "language_short.properties");
+			InputStream en_US = new FileInputStream(PATH
+					+ "language_short.properties");
 			props.load(en_US);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -135,13 +146,14 @@ public class NewPropertiesTest extends TestCase {
 		assertTrue("No se encontraron todas las claves esperadas", props
 				.keySet().size() == 678);
 	}
-	
+
 	@Test
 	public void test_en_GB_com_bannersIni() {
 		assertTrue("No se pudo cargar el archivo", simpleProperties != null);
 		PropertiesFile props = new PropertiesFile();
 		try {
-			InputStream en_GB=new FileInputStream(PATH + "en-GB.com_banners.ini");
+			InputStream en_GB = new FileInputStream(PATH
+					+ "en-GB.com_banners.ini");
 			props.load(en_GB);
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -150,13 +162,14 @@ public class NewPropertiesTest extends TestCase {
 		assertTrue("No se encontraron todas las claves esperadas", props
 				.keySet().size() == 37);
 	}
-	
+
 	@Test
 	public void test_en_GB_com_contactIni() {
 		assertTrue("No se pudo cargar el archivo", simpleProperties != null);
 		PropertiesFile props = new PropertiesFile();
 		try {
-			InputStream en_GB=new FileInputStream(PATH + "en-GB.com_contact.ini");
+			InputStream en_GB = new FileInputStream(PATH
+					+ "en-GB.com_contact.ini");
 			props.load(en_GB);
 		} catch (IOException e) {
 			e.printStackTrace();
