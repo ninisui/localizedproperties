97b682ee55f04acf3277033ec624440d4ca91a36
flores.leonardo@gmail.com <flores.leonardo@gmail.com@986cc4a0-e254-0410-b11a-59b74a07c1a1>
2012-11-07 11:40:20 +0000

    Version 0.8.5 has been released for helios

---
+++ branches/20121107_0_8_5/com.triadsoft.properties.editor/src/com/triadsoft/properties/preferences/PreferenceConstants.java
@@ -0,0 +1,22 @@
+package com.triadsoft.properties.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ * 
+ * @author Triad (flores.leonardo@gmail.com)
+ */
+public class PreferenceConstants {
+
+	public static final String WILDCARD_PATHS_ID = "preferences.wildcards";
+	public static final String WILDCARD_PATHS_PREFERENCES = WILDCARD_PATHS_ID+".path";
+	
+	//public static final String WILDCARD_PATH_SEPARATOR_PREFERENCES = "preferences.wilcards.separator";
+	public static final String WILDCARD_PATH_DEFAULT_INDEX_PREFERENCES = "preferences.wilcards.default.index";
+
+	public static final String LANGUAGE_CONTENT_TYPE_PREFERENCES = "preferences.languageContentType";
+	public static final String LANGUAGE_CONTENT_TYPE_SEPARATOR_PREFERENCES = "preferences.languageContentType.separator";
+
+	public static final String KEY_VALUE_SEPARATORS_PREFERENCES = "preferences.keyvalue.separators";
+	public static final String KEY_VALUE_DEFAULT_SEPARATOR_PREFERENCES = "preferences.separator.default";
+
+}
